84621654d60cbae829582a128e5529d4855db450
Pritam Sen <pritamsen@gmail.com>
2020-06-07 13:54:23 +0200

    Non-owner of the image can edit
    Non-owner of the image can edit

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -93,12 +93,15 @@ public class ImageController {
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, Model model, HttpSession session) {
         Image image = imageService.getImage(imageId);
-
+        User user=(User)session.getAttribute("loggeduser");
+        String error = "Only the owner of the image can edit the image";
         String tags = convertTagsToString(image.getTags());
         model.addAttribute("image", image);
         model.addAttribute("tags", tags);
+        model.addAttribute("editError",error);
+
         return "images/edit";
     }
 
